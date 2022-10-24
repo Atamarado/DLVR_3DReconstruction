4ddaeec386aacfa8f9099145217a95e1e9f80ff7
Marc Johler <m.a.johler@gmail.com>
2022-10-24 12:51:33 +0200

    implemented patch functionality for matrices

---
+++ Example.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 24 10:52:07 2022
+
+@author: Marc Johler
+"""
+
+import numpy as np
+import tensorflow as tf
+from PatchNet_tf import Encoder_common, Decoder, PatchNet
+from Patching import patch_matrix
+
+batch_size = 32
+patch_size = 128
+min_channels = 32
+
+input_size = (batch_size, patch_size, patch_size, 3)
+encoder_common = Encoder_common(input_size, min_channels)
+
+encoded_size = (batch_size, int(patch_size / 32), int(patch_size / 32), min_channels * 8)
+depth_decoder = Decoder(encoded_size, min_channels, 1, "depth_decoder")
+normals_decoder = Decoder(encoded_size, min_channels, 3, "normals_decoder")
+
+patch_net = PatchNet(batch_size, patch_size, min_channels)
+
+
+import cv2
+from matplotlib import pyplot as plt
+from Patching import patching
+
+car = cv2.imread("images/car.png")
+plane = cv2.imread("images/plane.png")
+  
+plt.imshow(car)
+#plt.imshow(plane)
+         
+# test patches with simple numbers
+numfield = np.array([[(10 * j) + i for i in range(0, 10)] for j in range(0, 10)])
+patches, _, _ = patch_matrix(numfield, 4, 3, 3)
+
+# test patches with car picture
+car_patches, _, _ = patch_matrix(car[:,:,2], 80, 3, 3)
+
+
+fig, axes = plt.subplots(nrows=3, ncols=3)
+plt.tight_layout()
+for i in range(3):
+    for j in range(3):
+        axes[i][j].imshow(car_patches[i * 3 + j], cmap = "binary_r")
+               
+plt.show()
---
+++ Patching.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 24 10:58:19 2022
+
+@author: Marc Johler
+"""
+
+import numpy as np
+
+def patch_matrix(img_matrix, patch_size, n_height, n_width):
+    height, width = img_matrix.shape
+    # compute the number of pixels after which a new patch is necessary
+    height_ratio = height / n_height
+    width_ratio = width / n_width
+    # compute the cutting points
+    # if there are more than one patch necessary
+    height_cuts = np.array([height_ratio * i for i in range(0, n_height - 1)])
+    width_cuts = np.array([width_ratio * i for i in range(0, n_width - 1)])
+    
+    # create the patches 
+    patches = np.repeat(None, n_height * n_width)
+    for h in range(n_height - 1):
+        height_cut_start = int(np.round(height_cuts[h]))
+        height_cut_end = height_cut_start + patch_size
+        # patches in the interior of the picture
+        for w in range(n_width - 1):
+            width_cut_start = int(np.round(width_cuts[w]))
+            patches[h * n_width + w] = img_matrix[height_cut_start:height_cut_end, width_cut_start:(width_cut_start + patch_size)]
+        # last patch in width
+        patches[(h + 1) * n_width - 1] = img_matrix[height_cut_start:height_cut_end, -patch_size:]
+    # last patches in height
+    for w in range(n_width - 1):
+        width_cut_start = int(np.round(width_cuts[w]))
+        patches[(n_height - 1) * n_width + w] = img_matrix[-patch_size:, width_cut_start:(width_cut_start + patch_size)]
+    # last patch in the lower right corner
+    patches[-1] = img_matrix[-patch_size:,-patch_size:]
+    
+    return patches, height_cuts, width_cuts
+    
+
+def patching(img, patch_size):
+    height, width, channels = img.shape
+    
+    output = np.repeat(None, channels)
+    
+    # compute number of necessary patches in each dimension
+    n_height = np.ceil(height / patch_size)
+    n_width = np.ceil(width / patch_size)
+    
+    for i in range(channels):
+        img_chan = img[:,:,i]
+        
+    
