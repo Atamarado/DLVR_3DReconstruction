a74380ea51dbb6ffd38cc50f1ada3f74a06a6948
Marc Johler <m.a.johler@gmail.com>
2022-10-24 14:40:33 +0200

    remember patch intervals

--- Patching.py
+++ Patching.py
@@ -19,23 +19,41 @@ def patch_matrix(img_matrix, patch_size, n_height, n_width):
     
     # create the patches 
     patches = np.repeat(None, n_height * n_width)
+    height_intervals = np.repeat(None, n_height * n_width) 
+    width_intervals = np.repeat(None, n_height * n_width)
     for h in range(n_height - 1):
         height_cut_start = int(np.round(height_cuts[h]))
         height_cut_end = height_cut_start + patch_size
         # patches in the interior of the picture
         for w in range(n_width - 1):
             width_cut_start = int(np.round(width_cuts[w]))
-            patches[h * n_width + w] = img_matrix[height_cut_start:height_cut_end, width_cut_start:(width_cut_start + patch_size)]
+            width_cut_end = width_cut_start + patch_size
+            index = h * n_width + w
+            patches[index] = img_matrix[height_cut_start:height_cut_end, width_cut_start:width_cut_end]
+            # save the intervals of the pixels (necessary for stitching later)
+            height_intervals[index] = np.array([height_cut_start, height_cut_end])
+            width_intervals[index] = np.array([width_cut_start, width_cut_end])
         # last patch in width
-        patches[(h + 1) * n_width - 1] = img_matrix[height_cut_start:height_cut_end, -patch_size:]
+        index = (h + 1) * n_width - 1
+        patches[index] = img_matrix[height_cut_start:height_cut_end, -patch_size:]
+        # save the intervals of the pixels (necessary for stitching later)
+        height_intervals[index] = np.array([height_cut_start, height_cut_end])
+        width_intervals[index] = np.array([width - patch_size, width])
     # last patches in height
     for w in range(n_width - 1):
         width_cut_start = int(np.round(width_cuts[w]))
-        patches[(n_height - 1) * n_width + w] = img_matrix[-patch_size:, width_cut_start:(width_cut_start + patch_size)]
+        width_cut_end = width_cut_start + patch_size
+        index = (n_height - 1) * n_width + w
+        patches[index] = img_matrix[-patch_size:, width_cut_start:width_cut_end]
+        # save the intervals of the pixels (necessary for stitching later)
+        height_intervals[index] = np.array([height - patch_size, height])
+        width_intervals[index] = np.array([width_cut_start, width_cut_end])
     # last patch in the lower right corner
     patches[-1] = img_matrix[-patch_size:,-patch_size:]
+    height_intervals[-1] = np.array([height - patch_size, height])
+    width_intervals[-1] = np.array([width - patch_size, width])
     
-    return patches, height_cuts, width_cuts
+    return patches, height_intervals, width_intervals
     
 
 def patching(img, patch_size):
@@ -47,13 +65,13 @@ def patching(img, patch_size):
     n_height = int(np.ceil(height / patch_size))
     n_width = int(np.ceil(width / patch_size))
     
-    height_cuts = None
-    width_cuts = None
+    height_intervals = None
+    width_intervals = None
     patches = np.repeat(None, channels)
     
     for i in range(channels):
-        patches[i], height_cuts, width_cuts = patch_matrix(img[:,:,i], patch_size, n_height, n_width)
+        patches[i], height_intervals, width_intervals = patch_matrix(img[:,:,i], patch_size, n_height, n_width)
     
-    return patches, height_cuts, width_cuts
+    return patches, height_intervals, width_intervals
         
     
