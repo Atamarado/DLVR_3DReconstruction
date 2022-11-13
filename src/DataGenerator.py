54736829b7d16e9e7b7481f645be3379faaef54e
Atamarado <52821119+Atamarado@users.noreply.github.com>
2022-11-13 12:59:10 +0100

    Data generation without patching completed

--- src/DataGenerator.py
+++ src/DataGenerator.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import os
 import numpy as np
+from PIL import Image
 
 # Inspired by: https://medium.com/analytics-vidhya/write-your-own-custom-data-generator-for-tensorflow-keras-1252b64e41c3
 class DataGenerator(tf.keras.utils.Sequence):
@@ -9,11 +10,15 @@ class DataGenerator(tf.keras.utils.Sequence):
                  batch_size,
                  shuffle=True,
                  validation=False,
-                 train_val_split=0.2):
+                 train_val_split=0.2,
+                 patching=True,
+                 patch_size=128):
 
         self.batch_size = batch_size
         self.validation = False
         self.train_val_split = train_val_split
+        self.patching = patching
+        self.patch_size = patch_size
 
         self.imagePath = os.path.join(path, 'images/')
         self.depthPath = os.path.join(path, 'depth_maps/')
@@ -33,21 +38,34 @@ class DataGenerator(tf.keras.utils.Sequence):
         pass
     
     def __get_input(self, name):
-        try:
-            return np.load(os.path.join(self.imagePath, name+".npy"))
-        except:
-            print("Hola")
+        img = Image.open(os.path.join(self.imagePath, name+".tiff"))
+        return np.array(img)
     
     def __get_data(self, batches):
         # Generates data containing batch_size samples
         X_batch = np.asarray([self.__get_input(name) for name in batches])
         y_batch = np.asarray([self.__get_output(name) for name in batches])
-        return X_batch, y_batch
+
+        # X_batch: Dimensions: (224, 224, 3): The base image in RGB. Range (0, 255)
+        # y_batch: Dimensions: (224, 224, 4): Depth (y_batch[:, :, 0]) and normal (y_batch[:, :, 1:4]) maps
+
+        if self.patching:
+            pass
+            # TODO: Implement patching functions and normalize each patch. Get foreground mask. See comment above
+        else:
+            return X_batch, y_batch
     
     def __get_output(self, name):
-        depth = np.load(os.path.join(self.depthPath, name+".npy"))
-        normal = np.load(os.path.join(self.normalPath, name+".npy"))
-        return depth, normal
+        depth = np.load(os.path.join(self.depthPath, name+".npz"))
+        normal = np.load(os.path.join(self.normalPath, name+".npz"))
+
+        depth = depth.f.depth
+        normal = normal.f.normals
+
+        # Concatenate both arrays. We need to reshape depth from (w x h) to (w x h x 1)
+        depth = np.reshape(depth, (np.shape(depth)[0], np.shape(depth)[1], 1))
+        conc = np.concatenate((depth, normal), axis=2)
+        return conc
 
     def __getitem__(self, index):
         batches = self.objs[index * self.batch_size:(index + 1) * self.batch_size]
--- src/testGenerator.py
+++ src/testGenerator.py
@@ -1,7 +1,7 @@
 from DataGenerator import DataGenerator
 
 if __name__ == "__main__":
-    datagen = DataGenerator('preprocess/data/pnData', 32)
+    datagen = DataGenerator('preprocess/data/pnData', 32, patching=False)
     X_batch, y_batch = datagen.__getitem__(1)
 
-    print(X_batch)
+    print(X_batch, y_batch)
