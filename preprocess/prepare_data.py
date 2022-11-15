a594fc5e43e10b64440eb6546c9f194b3d227e50
Atamarado <52821119+Atamarado@users.noreply.github.com>
2022-11-15 14:12:16 +0100

    Train/test split made in prepare_data

--- preprocess/prepare_data.py
+++ preprocess/prepare_data.py
@@ -1,6 +1,10 @@
 import os
 import shutil
 import re
+from sklearn.model_selection import train_test_split
+
+TEST_SIZE = 0.2
+SEED = 6
 
 def create_empty_folder(path):
     if os.path.isdir(path):
@@ -9,15 +13,18 @@ def create_empty_folder(path):
 
 def main():
     ORIGIN_FOLDER = 'data/textureless_deformable_surfaces'
-    DEST_FOLDER = 'data/pnData'
+    TRAIN_DEST_FOLDER = 'data/pnData/train'
+    TEST_DEST_FOLDER = 'data/pnData/test'
     INFO_TYPE = ['images', 'depth_maps', 'normals']
 
     PATCH_SIZE = 128
 
     # Create folder structure
-    create_empty_folder(DEST_FOLDER)
+    create_empty_folder(TRAIN_DEST_FOLDER)
+    create_empty_folder(TEST_DEST_FOLDER)
     for i in INFO_TYPE:
-        create_empty_folder(os.path.join(DEST_FOLDER, i))
+        create_empty_folder(os.path.join(TRAIN_DEST_FOLDER, i))
+        create_empty_folder(os.path.join(TEST_DEST_FOLDER, i))
 
     categoryList = os.listdir(ORIGIN_FOLDER)
     # For each object category
@@ -37,11 +44,16 @@ def main():
                 print("\t\tPow", pow)
                 powPath = os.path.join(iTypePath, pow)
                 rootName = "_".join([cat, pow])
-                baseDestPath = os.path.join(os.path.join(DEST_FOLDER, iType), rootName)
+
+                baseTrainDestPath = os.path.join(os.path.join(TRAIN_DEST_FOLDER, iType), rootName)
+                baseTestDestPath = os.path.join(os.path.join(TEST_DEST_FOLDER, iType), rootName)
 
                 fileList = os.listdir(powPath)
-                for file in fileList:
-                    shutil.copy(os.path.join(powPath, file), baseDestPath+re.sub(".*_", "_", file))
+                trainList, testList = train_test_split(fileList, test_size=TEST_SIZE, random_state=SEED)
+                for file in trainList:
+                    shutil.copy(os.path.join(powPath, file), baseTrainDestPath+re.sub(".*_", "_", file))
+                for file in testList:
+                    shutil.copy(os.path.join(powPath, file), baseTestDestPath+re.sub(".*_", "_", file))  
 
                 # # For each file in the filelist
                 # if iType == "images":
