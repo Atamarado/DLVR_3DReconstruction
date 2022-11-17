5e7f707a5b8752f00f23d36d1d4620fb6dff178b
Atamarado <52821119+Atamarado@users.noreply.github.com>
2022-11-17 10:18:48 +0100

    Data Generator testing with local path

--- src/testGenerator.py
+++ src/testGenerator.py
@@ -2,8 +2,8 @@ from DataGenerator import DataGenerator
 import os
 from matplotlib import pyplot as plt
 
-data_path = "C:\\Users\\xZoCk\\DLVR_3DReconstruction\\preprocess\\data\\pnData\\train"
-os.chdir(data_path)
+data_path = "preprocess\\data\\pnData\\train"
+# os.chdir(data_path)
 
 if __name__ == "__main__":
     datagen = DataGenerator(data_path, 32, patching=False)
