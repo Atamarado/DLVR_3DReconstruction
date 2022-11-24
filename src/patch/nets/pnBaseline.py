ead6f49218ec282134f372253aa2674b1de43e3b
Atamarado <52821119+Atamarado@users.noreply.github.com>
2022-11-24 11:28:57 +0100

    Baseline batch dimension fixed

--- src/patch/nets/pnBaseline.py
+++ src/patch/nets/pnBaseline.py
@@ -5,13 +5,14 @@ Created on Sun Oct 23 19:19:22 2022
 @author: Marc Johler
 """
 import tensorflow as tf
-from tensorflow.keras.layers import MaxPool2D, UpSampling2D, Conv2D
+from tensorflow.keras.layers import MaxPool2D, UpSampling2D, Conv2D, Input
 from patch.nets.PatchInterface import ConvLayer, PatchInterface
     
 class Encoder_common(tf.Module):
     def __init__(self, input_size, min_channels, name = "Encoder_common"):
         super(Encoder_common, self).__init__(name)
         self.layers = tf.keras.Sequential([
+            Input(input_size),
             ConvLayer(min_channels),
             ConvLayer(min_channels),
             MaxPool2D(2),
@@ -70,7 +71,7 @@ class Decoder(tf.Module):
 
 class TfNetwork(PatchInterface):
     def __init__(self, patch_size, min_channels):
-        input_size = (3, patch_size, patch_size, 3)
+        input_size = (patch_size, patch_size, 3)
         encoded_size = (3, int(patch_size / 32), int(patch_size / 32), min_channels * 8)
         self.encoder = Encoder_common(input_size, min_channels)
         self.depth_decoder = Decoder(encoded_size, min_channels, 1, "depth_decoder")
