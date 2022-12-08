9fac31bb4909a87ba4183e99cecef337183fb4a2
Marc Johler <m.a.johler@gmail.com>
2022-12-08 10:27:25 +0100

    some more comments

--- src/Feed_data.py
+++ src/Feed_data.py
@@ -10,7 +10,28 @@ import numpy as np
 import tensorflow as tf
 from tqdm import tqdm
 
+# Function to loop over a number of batches from the data_generator
 def patch_loop(model, data_generator, validation = False, n_batches = math.inf):
+    """
+    Parameters
+    ----------
+    model : PatchNet object
+        model which shall be trained/validated
+    data_generator : DataGenerator object
+        data generator which is used for loading and preparing the data
+    validation : boolean, optional
+        If TRUE uses the validation data. Otherwise uses the training data. The default is False.
+    n_batches : integerish, optional
+        If it is set lower than the maximum number of batches, only uses the first
+        n_batches of the DataGenerator. The default is math.inf.
+
+    Returns
+    -------
+    loss per patch
+        the combined depth and normals loss averaged over the total number of patches
+        in all the batches (metric for performance comparison)
+
+    """
     # set the options for the data generator
     data_generator.set_validation(validation)
     data_generator.set_patching(True)
@@ -39,8 +60,35 @@ def patch_loop(model, data_generator, validation = False, n_batches = math.inf):
         
     return loss / total_patches
 
-
+# function to loop over a set number of batches; will return depth and normals
+# loss seperately
 def patch_loop_separate_loss(model, data_generator, validation=False, n_batches=math.inf):
+    """
+    Parameters
+    ----------
+    model : PatchNet object
+        model which shall be trained/validated
+    data_generator : DataGenerator object
+        data generator which is used for loading and preparing the data
+    validation : boolean, optional
+        If TRUE uses the validation data. Otherwise uses the training data. The default is False.
+    n_batches : integerish, optional
+        If it is set lower than the maximum number of batches, only uses the first
+        n_batches of the DataGenerator. The default is math.inf.
+    
+    Returns
+    -------
+    loss per patch
+        the summed depth and normals loss averaged over the total number of patches
+        in all the batches (metric for performance comparison)
+    depth loss per patch
+        the depth loss averaged over the total number of patches
+        in all the batches (metric for performance comparison)
+    normals loss per patch
+        the normals loss averaged over the total number of patches
+        in all the batches (metric for performance comparison)
+    
+    """
     # set the options for the data generator
     data_generator.set_validation(validation)
     data_generator.set_patching(True)
@@ -77,7 +125,26 @@ def patch_loop_separate_loss(model, data_generator, validation=False, n_batches=
 
     return total_loss / total_patches, depth_loss / total_patches, normal_loss / total_patches
 
+# loops over a given number of batches of images
 def image_loop(model, data_generator, n_batches):
+    """
+    Parameters
+    ----------
+    model : PatchNet object
+        model which shall be trained/validated
+    data_generator : DataGenerator object
+        data generator which is used for loading and preparing the data
+    n_batches : integerish, optional
+        If it is set lower than the maximum number of batches, only uses the first
+        n_batches of the DataGenerator. The default is math.inf.
+
+    Returns
+    -------
+    loss per image
+        the summed depth and normals loss averaged over the total number of images 
+        in the batches (metric for performance validation/tests)
+
+    """
     # set the options for the data generator
     data_generator.set_validation(True)
     data_generator.set_patching(False)
@@ -100,7 +167,33 @@ def image_loop(model, data_generator, n_batches):
     return loss / np.min([data_generator.n_val, n_batches * batch_size])
         
 
+# Top-level function for training a PatchNet model
 def train(model, data_generator, epochs, n_batches = None, n_train_batches = None, n_val_batches = None):
+    """
+    Parameters
+    ----------
+    model : PatchNet object
+        model which shall be trained/validated
+    data_generator : DataGenerator object
+        data generator which is used for loading and preparing the data
+    epochs : integerish
+        The number of epochs for which the model shall be trained
+    n_batches : integerish, optional
+        If it is set lower than the maximum number of batches, only uses the first
+        n_batches of the DataGenerator. The default is math.inf.
+    n_train_batches : integerish, optional
+        If it is set lower than the maximum number of TRAINING batches, only uses the first
+        n_batches of the DataGenerator for TRAINING. The default is math.inf.
+    n_val_batches : integerish, optional
+        If it is set lower than the maximum number of VALIDATION batches, only uses the first
+        n_batches of the DataGenerator for VALIDATION. The default is math.inf.
+
+    Returns
+    -------
+    None.
+
+    """
+    # if the number of validation or training batches is not set, use the the common batch number 
     if n_val_batches is None and n_train_batches is None:
         n_val_batches = n_batches
         n_train_batches = n_batches
@@ -108,6 +201,7 @@ def train(model, data_generator, epochs, n_batches = None, n_train_batches = Non
     assert n_val_batches is not None
     assert n_train_batches is not None
     
+    # repeat the training and validation for 'epoch' times
     for epoch in range(epochs):
         train_loss = patch_loop(model, data_generator, validation = False, n_batches = n_train_batches)
         val_loss_patch = patch_loop(model, data_generator, validation = True, n_batches = n_val_batches)
@@ -121,7 +215,25 @@ def train(model, data_generator, epochs, n_batches = None, n_train_batches = Non
     print("Training finished after", epochs, "Epochs")
     
 
+# Top-level function for validation for testing of the model
 def test(model, data_generator, n_batches):
+    """
+    Parameters
+    ----------
+     model : PatchNet object
+         model which shall be trained/validated
+     data_generator : DataGenerator object
+         data generator which is used for loading and preparing the data
+     n_batches : integerish, optional
+         If it is set lower than the maximum number of batches, only uses the first
+         n_batches of the DataGenerator. The default is math.inf.
+
+    Returns
+    -------
+    test_loss : numeric
+        The average combined depth and normals loss per image
+
+    """
     test_loss = image_loop(model, data_generator, n_batches = n_batches)
     batch_size = data_generator.batch_size 
     print("Tested on", n_batches * batch_size, "images")
